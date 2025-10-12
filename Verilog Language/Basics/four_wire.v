b059ecbe643c92aeb6f0d164993cd4894b5e523d
cuz-whynot <99198758+cuz-whynot@users.noreply.github.com>
2025-10-12 15:57:01 -0700

    Create four_wire.v
    Used catenation instead of 4 assigns.

---
+++ Verilog Language/Basics/four_wire.v
@@ -0,0 +1,9 @@
+module top_module( 
+    input a,b,c,
+    output w,x,y,z );
+	assign w = a;
+    assign x = b;
+    assign y = b;
+    assign z = c;
+  assign {w, x, y, z} = {a, b, b, c} ;  // used concatenation for efficiency 
+endmodule
