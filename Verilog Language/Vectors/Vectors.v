7509ae391a8cbb35bafc714889f0727743b66cb2
cuz-whynot <99198758+cuz-whynot@users.noreply.github.com>
2025-10-12 23:39:33 -0700

    Update Vectors.v

--- Verilog Language/Vectors/Vectors.v
+++ Verilog Language/Vectors/Vectors.v
@@ -6,7 +6,5 @@ module top_module (
     output wire o0  ); // Module body starts after module declaration
 	
     assign outv = vec;
-    assign o0 = vec[0];
-    assign o1 = vec[1];
-    assign o2 = vec[2];
+    assign {o2, o1, o0} = vec; 
 endmodule
