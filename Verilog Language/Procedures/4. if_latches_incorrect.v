c744ae757c8c8cec716fe5e04e4c1fbfee71cbb8
cuz-whynot <99198758+cuz-whynot@users.noreply.github.com>
2026-01-31 22:33:32 -0700

    Update and rename 3. if_latches_incorrect.v to 4. if_latches_incorrect.v

--- Verilog Language/Procedures/4. if_latches_incorrect.v
+++ Verilog Language/Procedures/4. if_latches_incorrect.v
@@ -9,15 +9,13 @@ module top_module (
     always @(*) begin
         if (cpu_overheated)
            shut_off_computer = 1;
-        else 
-           shut_off_computer = 0;  
+        else shut_off_computer = 0;
     end
 
     always @(*) begin
         if (~arrived)
-           keep_driving = ~gas_tank_empty;
-        else
-           keep_driving = gas_tank_empty;
+           keep_driving = ~gas_tank_empty;    //destination is not arrived but tank is empty
+        else keep_driving = 0;                ///there is no need to drive if u've arrived to ur destination
     end
 
 endmodule
