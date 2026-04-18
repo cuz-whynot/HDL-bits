65fae8e06d440ca28b451eaf9d8f2d35e66b58e2
cuz-whynot <99198758+cuz-whynot@users.noreply.github.com>
2026-04-18 01:49:36 -0700

    Create 13.  dffs_and_gates.v

---
+++ Circuits/2. Sequential logic/1. Latches and flipflops/13.  dffs_and_gates.v
@@ -0,0 +1,16 @@
+module top_module (
+    input clk,
+    input x,
+    output z
+); 
+    logic d1, d2, d3, q1, q2, q3;
+	assign d1 = x ^ q1 ;
+    assign d2 = x & ~q2;
+    assign d3 = x | ~q3;
+    assign z = ~(q1 | q2 | q3);
+    always@(posedge clk) begin
+        q1 <= d1;
+        q2 <= d2;
+        q3 <= d3;
+    end
+endmodule
