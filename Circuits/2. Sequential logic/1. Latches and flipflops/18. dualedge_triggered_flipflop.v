8a1a554719899b30ad0a96f46e931daeb176fed4
cuz-whynot <99198758+cuz-whynot@users.noreply.github.com>
2026-06-18 01:40:57 -0700

    Update 18. dualedge_triggered_flipflop.v

--- Circuits/2. Sequential logic/1. Latches and flipflops/18. dualedge_triggered_flipflop.v
+++ Circuits/2. Sequential logic/1. Latches and flipflops/18. dualedge_triggered_flipflop.v
@@ -1,10 +1,11 @@
-module top_module (
-    input clk,
-    input d,
-    output q
-);
-	reg q_1, q_2;
-    always@(posedge clk) q_1 <= d;
-    always@(negedge clk) q_2 <= d;
-    assign q = clk? q_1: q_2;
+module top_module(
+	input clk,
+	input d,
+	output q);
+	reg p, n;
+    always @(posedge clk)
+        p <= d ^ n;
+    always @(negedge clk)
+        n <= d ^ p;
+    assign q = p ^ n;
 endmodule
